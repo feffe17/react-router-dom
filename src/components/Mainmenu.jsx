c2294aea7eefb80770b983797bb9f491d47e8974
feffe1702 <fpm.asd@gmail.com>
2024-11-25 11:25:15 +0100

    [FIX] navbar

--- src/components/Mainmenu.jsx
+++ src/components/Mainmenu.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
 
 export default function navbar() {
-    <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/books">Books</NavLink>
-        <NavLink to="/contacts">Contacts</NavLink>
-    </nav>
+    return (
+
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/books">Books</NavLink>
+            <NavLink to="/contacts">Contacts</NavLink>
+        </nav>
+    )
 }
