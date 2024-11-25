bb56401b1210c9591aa96e7222240842b0a3df59
feffe1702 <fpm.asd@gmail.com>
2024-11-25 10:21:10 +0100

    [WIP] add routes

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,20 @@
+export default function () {
+    return (
+        <>
+            <header>
+                <h1>title</h1>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/About">About</a>
+                    <a href="/Books">Books</a>
+                    <a href="/Contacts">Contacts</a>
+                </nav>
+            </header>
+            <main>
+                <div className="container">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis iste omnis, voluptatum fugit explicabo eos praesentium quae soluta reprehenderit dolore aut incidunt alias perferendis assumenda quos accusantium. Ut, delectus similique.
+                </div>
+            </main>
+        </>
+    )
+}
